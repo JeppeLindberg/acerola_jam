629a918fda3978a4b10f85c6a64b8609b3aaaf16
unknown <jepp3441@gmail.com>
2024-03-03 21:41:00 +0100

    progress

--- scripts/beam_network.gd
+++ scripts/beam_network.gd
@@ -4,6 +4,10 @@ extends Node2D
 var _main_scene
 var _world
 
+var _relay_to_id = {}
+var _id_to_relay = {}
+var _next_id = 0;
+
 @export_flags_2d_physics var negation_collision_layer
 
 
@@ -11,77 +15,63 @@ func _ready():
 	_main_scene = get_node('/root/main_scene')
 	_world = get_node('/root/main_scene/world')
 
-
 func update_beam_network():
 	var relays = _main_scene.get_children_in_groups(_world, ['relay'], true)
-	var relay_to_id = {}
-	var id_to_relay = {}
-	var next_id = 0;
+	_next_id = 0;
 	for relay in relays:
-		relay_to_id[relay] = next_id;
-		id_to_relay[next_id] = relay;
-		next_id += 1
+		_relay_to_id[relay] = _next_id;
+		_id_to_relay[_next_id] = relay;
+		_next_id += 1
 
 	var directional_network = {}
 
-	for id in id_to_relay.keys():
-		directional_network[id] = {'color': 'none', 'depth': 9999, 'point': id_to_relay[id].global_position, 'outgoing_edges': [], 'handled': false}
-		if id_to_relay[id].is_emitter:
-			directional_network[id] = {'color': 'blue', 'depth': 0, 'point': id_to_relay[id].global_position, 'outgoing_edges': [], 'handled': false}
+	for id in _id_to_relay.keys():
+		directional_network[id] = {'type': 'relay', 'color': 'none', 'depth': 9999, 'point': _id_to_relay[id].global_position, 'parents': []}
+		if _id_to_relay[id].is_emitter:
+			directional_network[id] = {'type': 'emitter', 'color': 'blue', 'depth': 0, 'point': _id_to_relay[id].global_position, 'parents': []}
 
-	var modified_directional_network = directional_network
-	var dirty = false;
+	directional_network = _create_parent_network(directional_network)
+	directional_network = _create_negated_network(directional_network)
+	directional_network = _colorize_network(directional_network)
 
-	while(true):
-		for id in directional_network.keys():
-			if modified_directional_network[id]['handled']:
-				continue;
-
-			var relay = id_to_relay.get(id, false)
-
-			if relay:
-				var color = directional_network[id]['color'];
-				if color == 'none':
-					continue;
-
-				for target_relay in id_to_relay[id].connected_relays:
-					if directional_network[relay_to_id[target_relay]]['depth'] <= directional_network[id]['depth']:
-						continue;
-
-					var negation_points = _get_beam_negation_points(id_to_relay[id], target_relay);
-					
-					dirty = true;
-
-					if len(negation_points) == 0:
-						modified_directional_network[id]['outgoing_edges'].append(relay_to_id[target_relay])
-						modified_directional_network[id]['handled'] = true;
-						modified_directional_network[relay_to_id[target_relay]]['color'] = color;
-						modified_directional_network[relay_to_id[target_relay]]['depth'] = modified_directional_network[id]['depth'] + 1;
-
-					var negations = 0;
-					var prev_id = 0;
-					for point in negation_points:
-						if negations == 0:
-							modified_directional_network[id]['outgoing_edges'].append(next_id);
-							modified_directional_network[id]['handled'] = true;
-							
-						if negations >= 1:
-							modified_directional_network[prev_id]['outgoing_edges'].append(next_id);
-
-						negations += 1;
-						modified_directional_network[next_id] = {'color': _calculate_negated_color(color, negations), 'depth': directional_network[id]['depth'], 'point': point, 'outgoing_edges': [], 'handled': true}
-						prev_id = next_id;
-						next_id += 1
-
-					modified_directional_network[relay_to_id[target_relay]]['color'] = _calculate_negated_color(color, negations);
-					modified_directional_network[relay_to_id[target_relay]]['depth'] = modified_directional_network[id]['depth'] + 1;
-		
-		if dirty:
-			directional_network = modified_directional_network;
-		else:
-			break;
-	
 	print(directional_network);
+
+func _create_parent_network(directional_network):
+	var handled = []
+
+	while true:
+		var frontier = []
+		for id in directional_network.keys():
+			if (directional_network[id]['depth'] != 9999) and !(handled.has(id)):
+				frontier.append(id)
+		
+		if len(frontier) == 0:
+			break;
+		
+		for id in frontier:
+			for target_relay in _id_to_relay[id].connected_relays:
+				if directional_network[id]['depth'] + 1 <= directional_network[_relay_to_id[target_relay]]['depth']:
+					directional_network[_relay_to_id[target_relay]]['parents'].append(id);				
+					directional_network[_relay_to_id[target_relay]]['depth'] = directional_network[id]['depth'] + 1;
+			
+			handled.append(id);
+	
+	return(directional_network);
+
+func _create_negated_network(directional_network):
+	var from_to_ids = {}
+
+	for id_to in directional_network:
+		for id_from in directional_network[id_to]['parents']:
+			from_to_ids[id_from] = id_to;
+
+	for id_from in from_to_ids.keys():
+		var id_to = from_to_ids[id_from];
+		var negation_points = _get_beam_negation_points(_id_to_relay[id_from], _id_to_relay[id_to])
+		if len(negation_points) > 0:
+			directional_network = _inject_negation_point(directional_network, id_from, id_to, negation_points[0])
+	
+	return(directional_network);
 
 func _get_beam_negation_points(relay_from, relay_to):
 	var result = []
@@ -110,6 +100,37 @@ func _get_beam_negation_points(relay_from, relay_to):
 			result.append(col_pos)
 	
 	return(result);
+
+func _inject_negation_point(directional_network, id_from, id_to, point):
+	directional_network[_next_id] = {'type': 'negation', 'color': 'none', 'depth': directional_network[id_from]['depth'], 'point': point, 'parents': [id_from]}
+	directional_network[id_to]['parents'].erase(id_from);
+	directional_network[id_to]['parents'].append(_next_id);
+	_next_id += 1;
+
+	return(directional_network);
+
+func _colorize_network(directional_network):
+	var dirty = false
+
+	while true:
+		dirty = false
+		
+		for id in directional_network:
+			if directional_network[id]['color'] == 'none':
+				if len(directional_network[id]['parents']) > 0:
+					var parent_id = directional_network[id]['parents'][0];
+
+					if directional_network[id]['type'] == 'negation':
+						directional_network[id]['color'] = _calculate_negated_color(directional_network[parent_id]['color'], 1);
+					else:
+						directional_network[id]['color'] = directional_network[parent_id]['color']
+
+					dirty = true;
+
+		if dirty == false:
+			break;
+	
+	return(directional_network);
 
 func _calculate_negated_color(base_color, negations):
 	var negate = negations % 2 == 1;
